ded7850396f3a3f9204dcebdf7288d48d4f2f63d
kyj0101 <fioonmp1001@gmail.com>
2021-08-11 17:37:54 +0900

    유효성검사도추가

--- src/main/resources/static/js/mypage/withdraw.js
+++ src/main/resources/static/js/mypage/withdraw.js
@@ -20,31 +20,39 @@ function withdrawBtn(){
 	var password = $("#password").val();
 	var reasonCode = $(".form-check-input:checked").val();
 	
+	//직접 입력일때
 	if(reasonCode == 'RN005'){
 		reasonCode = $("#reasonInput").val();
-	
-	}else{	
 		
+		//직접 입력하지 않았다면
+		if(reasonCode == ""){
+			alert("사유를 입력하세요.");
+			return;
+		}
+		
+	}else if(reasonCode == "" || password == ""){	
+		alert("사유 또는 비밀번호를 입력하세요.");
+	
+	}else{
+		
+		$.ajax({
+			type:"post",
+			url:"/member/mypage/withdraw",
+			data:{
+				"password":password,
+				"reasonCode":reasonCode
+			},
+			success(result) {
+				
+				if(result == "ok"){
+				
+					alert("탈퇴되었습니다.");
+					location.replace("/")
+				
+				}else{
+					alert("비밀번호가 일치하지 않습니다.");				
+				}
+			},
+		});
 	}
-	
-	$.ajax({
-		type:"post",
-		url:"/member/mypage/withdraw",
-		data:{
-			"password":password,
-			"reasonCode":reasonCode
-		},
-		success(result) {
-			
-			if(result == "ok"){
-			
-				alert("탈퇴되었습니다.");
-				location.replace("/")
-			
-			}else{
-				alert("탈퇴를 실패했습니다.");				
-			}
-		},
-	})
-	
 }
