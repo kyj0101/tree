8c8f520ca01b7fb2793b814570650c0aca007e8d
kyj0101 <fioonmp1001@gmail.com>
2021-09-30 13:40:15 +0900

    오류수정

--- src/main/java/com/vtex/tree/TreeApplication.java
+++ src/main/java/com/vtex/tree/TreeApplication.java
@@ -3,16 +3,11 @@ package com.vtex.tree;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-@EnableSwagger2
 @SpringBootApplication
 public class TreeApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TreeApplication.class, args);
 	}
-	
-	
-
 }
